0e3d131ee975102c038bac40d93906d2fe37fd14
aenigma-lab <f10.shubham@gmail.com>
2025-06-05 20:12:28 +0530

    multiple birthdays automail with datetime module"

--- Mini Projects/birthday_wisher/main.py
+++ Mini Projects/birthday_wisher/main.py
@@ -1,39 +1,36 @@
-import smtplib
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
 
 
-
-# my_email = "idealshubham1998@gmail.com"
-# passwd = "shubham123()"
-# with smtplib.SMTP("smpt.gmail.com") as connection:
-# ## connection = smtplib.SMTP("smtp.gmail.com")
-#     connection.starttls()
-#     connection.login(user=my_email, password=passwd)
-#
-#     connection.sendmail(from_addr=my_email, to_addrs="f10.shubham@gmail.com", msg="Subject: Hello from Shubham\n\nThis is the body of my email.")
-# ## connection.close()
-
-
-#Monday Motivation Project
-import smtplib
-import datetime as dt
+from datetime import datetime
+import pandas
 import random
+import smtplib
 
-MY_EMAIL = "shubham@gmail.com"
-MY_PASSWORD = "abcd1234()"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-    print(quote)
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=MY_EMAIL,
-            msg=f"Subject:Monday Motivation\n\n{quote}"
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
